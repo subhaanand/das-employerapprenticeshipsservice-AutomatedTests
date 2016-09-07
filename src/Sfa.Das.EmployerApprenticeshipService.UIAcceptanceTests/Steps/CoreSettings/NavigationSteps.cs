cfd962cf138ecbeead1fb3314b9a7b8489d93f5f
Leke Ibiyode <Akinleke@gmail.com>
2016-09-07 17:15:44 +0100

    navigation steps changed

--- src/Sfa.Das.EmployerApprenticeshipService.UIAcceptanceTests/Steps/CoreSettings/NavigationSteps.cs
+++ src/Sfa.Das.EmployerApprenticeshipService.UIAcceptanceTests/Steps/CoreSettings/NavigationSteps.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.IE;
-using OpenQA.Selenium.PhantomJS;
 using Sfa.Das.EmployerAprrenticeshipService.Pages;
 using Sfa.Das.EmployerAprrenticeshipService.Pages.AML;
 using Sfa.Das.EmployerAprrenticeshipService.Pages.Pirean;
